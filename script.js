c92ad91f8e895f532d95f46692588b7ab0f8729f
KPLUU <kph.luu@gmail.com>
2019-11-18 22:29:18 +0100

    third commit

--- script.js
+++ script.js
@@ -23,8 +23,61 @@ var burger = document.getElementsByClassName("navbar-toggler")[0];
 
 // 2. Define the function that will respond to the event.
 var onBurgerClick = function () {
-    var menuContent = document.getElementById("navbarHeader");
-    menuContent.classList.toggle("collapse");
+    var burgerContent = document.getElementById("navbarHeader");
+    burgerContent.classList.toggle("collapse");
 }
 // 3. Add the event listener for the element and function
 burger.addEventListener("click", onBurgerClick);
+
+// Fonctionnalité 3 : 
+//1. Find and store the element we want to listen to events on.
+var cardOne = document.getElementsByClassName("col-md-4")[0];
+var editButton = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[0];
+
+// 2. Define the function that will respond to the event.
+var onEditClick = function () {
+    var cardContent = document.getElementsByClassName("card-text")[0];
+    cardContent.style.color = "red" ;
+}
+// 3. Add the event listener for the element and function
+editButton.addEventListener("click", onEditClick);
+
+// Fonctionnalité 4 : 
+//1. Find and store the element we want to listen to events on.
+var cardTwo = document.getElementsByClassName("col-md-4")[1];
+console.log(cardTwo)
+var editButtonTwo = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[1];
+console.log(editButtonTwo)
+
+// 2. Define the function that will respond to the event.
+var onEditClick = function () {
+    var secondCard = document.getElementsByClassName("card-text")[1];
+    if (secondCard.style.color === 'green'){
+    secondCard.style.color = '' ;
+    }else {
+    secondCard.style.color = 'green' ;
+    }
+}
+   
+// 3. Add the event listener for the element and function
+editButtonTwo.addEventListener("click", onEditClick);
+
+// Fonctionnalité 5 : 
+//1. Find and store the element we want to listen to events on.
+var doubleClickerNavbar = document.getElementsByClassName("navbar navbar-dark bg-dark box-shadow")[0];
+console.log(doubleClickerNavbar)
+
+// 2. Define the function that will respond to the event.`
+var style = document.styleSheets[0]
+console.log(style)
+
+var onDoubleClick = function () {
+    if (document.styleSheets[0].disabled = true) {
+        document.styleSheets[0].disabled = '';
+      } else {
+        document.styleSheets[0].disabled = true;
+      }
+}
+
+// 3. Add the event listener for the element and function
+doubleClickerNavbar.addEventListener("dblclick", onDoubleClick);
